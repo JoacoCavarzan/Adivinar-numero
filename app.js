0d9d48cf2f639fda0ce82dc9ef395a3ba5c10110
Joaquin Cavarzan <joacocava@gmail.com>
2024-07-21 19:32:25 -0300

    random

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
+let numeroSecreto = generarNumeroSecreto();
+console.log(numeroSecreto);
+
 function asignarTextoElemento(elemento, texto){
     let elementoHTML = document.querySelector(elemento);
     elementoHTML.innerHTML = texto;
+    return;
 }
 
 function intentoDeUsuario() {
     alert('click boton')
+    return;
+}
+
+function generarNumeroSecreto(params) {
+    return Math.floor(Math.random()*10)+1;     
 }
 
 asignarTextoElemento('h1', 'Juego del número secreto!');
